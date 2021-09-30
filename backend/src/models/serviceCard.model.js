e14e89752265966b751273fc351c87f4f93df31d
Srikanth <srikanth.vs12@gmail.com>
2021-09-30 09:42:16 +0530

    serviceCard

--- backend/src/models/serviceCard.model.js
+++ backend/src/models/serviceCard.model.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const serviceCardSchema = new mongoose.Schema({
-    category: {type:String, required:true},
+    category: { type: String, required: true },
+    name:{type:String, required:true},
     image: {type:String, required:true},
-    review: {type:Number, required:true},
-    price: {type:String, required:true},
-    disclaimers: { type: String, required: true }
+    review: { type: Number, required: true },
+    totalReviews:{type: String, required: true},
+    price: {type:Number, required:true},
+    disclaimers: [{ type: String, required: true }]
 },{
     versionKey:false,
     timestamps:true
