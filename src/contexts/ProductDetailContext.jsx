5fcb760e54c1c109d7df350ca9800e40c96da662
Srikanth <srikanth.vs12@gmail.com>
2021-10-03 16:56:19 +0530

    cart context

--- src/contexts/ProductDetailContext.jsx
+++ src/contexts/ProductDetailContext.jsx
@@ -1,10 +1,11 @@
 import { createContext, useState } from "react";
 
-export const ProductDetailContext = createContext(()=>{},[])
+export const ProductDetailContext = createContext(()=>{},[],()=>{},"")
 
 function ProductDetailProvider({ children }) {
     // const [detail, setDetail] = useState({});
-    const [cart,setCart] = useState([]);
+    const [cart, setCart] = useState([]);
+    const [total, setTotal] = useState(0);
 
     // const handleDetail = (details) => {
     //     setDetail(details);
@@ -12,9 +13,12 @@ function ProductDetailProvider({ children }) {
     const handleCart = (cart) => {
         setCart(cart);
     }
+    const handleTotal = (total) => {
+        setTotal(total);
+    }
     
     return (
-        <ProductDetailContext.Provider value = {{cart,handleCart}}>{children}</ProductDetailContext.Provider>
+        <ProductDetailContext.Provider value = {{cart,handleCart,total,handleTotal}} >{children}</ProductDetailContext.Provider>
     )
 }
 
