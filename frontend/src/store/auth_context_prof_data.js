011edb5a3d03ec38b806000c26a95c3fd537c8b3
Ankit Kumar <ankitkumar3443@gmail.com>
2021-10-03 20:21:16 +0530

    v1 completed

--- frontend/src/store/auth_context_prof_data.js
+++ frontend/src/store/auth_context_prof_data.js
@@ -3,20 +3,37 @@ import React,{useState,useEffect} from "react";
 let url_data = new URLSearchParams(window.location.search)
 
 let proessionDetails = async (g) => {
-  // console.log(url_data.get('category'));
+ 
+    try {
+       // console.log(url_data.get('category'));
     // let data = await fetch("http://localhost:3333/serviceProfessional/?category=Electricians");
     let data = await fetch(`http://localhost:3333/serviceProfessional/?city=${url_data.get("city")}&category=${url_data.get("category")}`);
     data = await data.json();
     g(data)
     return data;
+    }
+    catch(err) {
+      console.log(err);
+     console.log('backend is not started');
+      return {}
+    }
   };
 let professionalProfileDetails = async (g) => {
-    // let data = await fetch("http://localhost:3333/professionalProfile/?category=Electrician");
+   
+    try {
+      // let data = await fetch("http://localhost:3333/professionalProfile/?category=Electrician");
     let data = await fetch(`http://localhost:3333/professionalProfile/?category=${url_data.get("category")}`);
 
     data = await data.json();
     g(data)
     return data;
+   }
+   catch(err) {
+     console.log(err);
+     console.log('backend is not started');
+
+     return {}
+   }
   };
 
 let init=[{
